637c1d735bcda5237ed00123f535395bc9593797
aposdroid <aposdroid@gmail.com>
2023-01-15 22:30:10 +0300

    B BunTest перенёс создание булки внутрь тестов

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -4,15 +4,16 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class BunTest {
-    Bun bun = new Bun("Флюоресцентная булка R2-D3", 988);
 
     @Test
     public void bunGetName() {
+        Bun bun = new Bun("Флюоресцентная булка R2-D3", 988);
         Assert.assertEquals("Флюоресцентная булка R2-D3", bun.getName());
     }
 
     @Test
     public void getPrice() {
+        Bun bun = new Bun("Флюоресцентная булка R2-D3", 988);
         Assert.assertEquals(988, bun.getPrice(),0);
     }
 }
